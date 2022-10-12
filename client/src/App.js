1d54806d65f5b7fd57ca8852ef8f0a351c2da4a2
andriibeha <beha.andrii@gmail.com>
2022-10-12 16:09:04 +0300

    add start file structure

---
+++ client/src/App.js
@@ -0,0 +1,11 @@
+
+import "./scss/app.scss"
+
+
+const App = () => {
+  return (
+    <div className="container">APP</div>
+  );
+};
+
+export default App;
---
+++ client/src/redux/slices/tickers Slice.js
@@ -0,0 +1,45 @@
+import { createSlice, createAsyncThunk } from '@reduxjs/toolkit'
+import axios from "axios";
+
+const initialState = {
+    items: [],
+    status: 'loading',
+};
+
+export const fetchTickers = createAsyncThunk(
+    'tickers/fetchTickers',
+    async () => {
+        const { data } = await axios.get(``);
+        return data
+    }
+);
+
+export const tickersSlice = createSlice({
+    name: 'tickers',
+    initialState,
+    reducers: {
+        setItems(state, action) {
+            state.items = action.payload;
+        },
+    },
+    extraReducers: {
+        [fetchTickers.pending]: (state, action) => {
+            state.status = 'loading';
+            state.items = [];
+        },
+        [fetchTickers.fulfilled]: (state, action) => {
+            state.items = action.payload;
+            state.status = 'success';
+        },
+        [fetchTickers.rejected]: (state, action) => {
+            state.status = 'error';
+            state.items = [];
+        },
+    },
+});
+
+/* export const selectTickerData = (state) => state.rickers; */
+
+export const { setItems } = tickersSlice.actions;
+
+export default tickersSlice.reducer;
